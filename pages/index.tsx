b81967359d664ce9074ddfe37cf0776b9b93fbb6
Dary Mahdi <darmah2509093@gmail.com>
2024-01-31 17:17:24 +0700

    add

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,25 @@
 import type { NextPage } from "next";
 import Table from "../components/layout/Table";
 import styles from "../styles/Home.module.css";
+import { useState, useEffect } from 'react'
 import Card from "../components/layout/Card";
 
-const Home: NextPage = () => {
+import {show} from "../services/todoService";
+import { componentShapes } from "react-daisyui/dist/constants";
+
+
+export default function Home(){
+  const data = useEffect(() => {
+    fetch('/api/todo')
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.data)
+    })
+  }, []);
+  
   return (
     <div className={styles.container}>
       <div className="grid grid-cols-3 gap-4 my-3">
-
 
 
       <Card title="Created" actionButtonLabel={"Created"} secondaryButtonLabel={"Hello World"}>
@@ -26,5 +38,3 @@ const Home: NextPage = () => {
    </div>
   );
 };
-
-export default Home;
